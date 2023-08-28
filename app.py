d6c5e83df39e49a17069eed9e8bb2335be77dfed
Ato Aidoo <77356776+atoaidoocr7@users.noreply.github.com>
2023-08-28 19:24:56 -0400

    Add parse response functionality

--- app.py
+++ app.py
@@ -1,48 +1,96 @@
 
 # API doc https://courses.ianapplebaum.com/public/docs/
 import requests
+import json
+
+
+class Event:
+
+    def __init__(self, eventId, syllabus_id, event_name, event_description,
+                 event_date, created_at, updated_at, class_type):
+        self.eventId = eventId
+        self.syllabus_id = syllabus_id
+        self.event_name = event_name
+        self.event_description = event_description
+        self.event_date = event_date
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.class_type = class_type
+
+    def __str__(self):
+        formattedStr = ("Event Name: {}\nEvent Description: {}\nCreated At : {}\nUpdated At: {}"
+                        ).format(self.event_name, self.event_description, self.created_at, self.updated_at)
+        return formattedStr
+
+
+# Open and read the JSON file
+with open('credentials.json', 'r') as json_file:
+    data = json.load(json_file)
 
 # API SETUP
 URL = "https://courses.ianapplebaum.com"
 syllabusByIdApi = "/syllabus/"
 syllabusApi = "/syllabus/"
 userApi = "/user/"
-key = "QUtKvrHyNHmaBQAeblqzlBd8BAmyn6HSVNrIfzb8"
+API_KEY = data['API_KEY']
 
 headers = {
-    'Authorization': "Bearer {QUtKvrHyNHmaBQAeblqzlBd8BAmyn6HSVNrIfzb8}",
+    'Authorization': "Bearer {API_KEY}",
     'Content-Type': "application.json",
     'Accept': "application/json"
 }
 
 # Get request for api/syllabus/{id}
 # Gets a syllabus for a specific semester by (ID)
+
+
 def getSyllabusById():
-    fallSemesterId = "4"                                                                # Seems to be the ID number to return the fall 2023 info
-    response = requests.get(URL + syllabusByIdApi + fallSemesterId, headers=headers).json()
-    #print(response)
+    # Seems to be the ID number to return the fall 2023 info
+    fallSemesterId = "4"
+    response = requests.get(URL + syllabusByIdApi +
+                            fallSemesterId, headers=headers).json()
+    # print(response)
     return response
 
 # Get request for api/syllabus/
 # Retrieves every syllabus for every semester
+
+
 def getSyllabus():
     response = requests.get(URL + syllabusApi, headers=headers).json()
     print(response)
 
 # Get request for api/user/
 # Not working yet
+
+
 def getUser():
     response = requests.get(URL + userApi, headers=headers).json()
     print(response)
 
 # TODO Parse the json here
+# Returns an array of event objects
+
+
 def parseSyllabus(response):
-    print(response)
+
+    events = response['events']
+    eventObjects = []
+    for event in events:
+        eventId, syllabus_id, event_name = event['id'], event['syllabus_id'], event['event_name']
+        event_desc, event_date, created_at = event["event_description"], event["event_date"], event["created_at"]
+        updated_at, class_type = event['updated_at'], event['class_type']
+
+        eventObjects.append(
+            Event(eventId, syllabus_id, event_name, event_desc, event_date, created_at, updated_at, class_type))
+
+    for eventObj in eventObjects:
+        print(eventObj)
+        print("---------------------------\n")
 
 
 if __name__ == '__main__':
     response = getSyllabusById()
     parseSyllabus(response)
-    #getSyllabus()
-    #getUser()
-
+    # getSyllabus()
+    # getUser()
