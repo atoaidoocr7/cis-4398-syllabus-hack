06daa1e28ac272b4fc92ad508cfea05f5b2eef5c
mars123123 <tul51449@temple.edu>
2023-08-28 17:22:31 -0400

    added api get

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# API doc https://courses.ianapplebaum.com/public/docs/
+import requests
 
+# API SETUP
+URL = "https://courses.ianapplebaum.com"
+api = "/syllabus/1"
+#userHeader = "/api/user"
+key = "QUtKvrHyNHmaBQAeblqzlBd8BAmyn6HSVNrIfzb8"
 
-print("CIS 4398 starter hack")
+headers = {
+    'Authorization': "Bearer {QUtKvrHyNHmaBQAeblqzlBd8BAmyn6HSVNrIfzb8}",
+    'Content-Type': "application.json",
+    'Accept': "application/json"
+}
+
+#Sylabus Get
+response = requests.get(URL+api, headers=headers).json()
+
+print(response)
